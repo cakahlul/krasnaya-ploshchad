57a172e4529e706a8a4b4034a6c9fe4388c2dc8a
Ahlul Esasjana <cakahlul@gmail.com>
2025-05-08 15:49:12 +0700

    add cors config

--- apps/aioc-service/src/main.ts
+++ apps/aioc-service/src/main.ts
@@ -3,8 +3,13 @@ import { AppModule } from './app.module';
 import { FirebaseAuthGuard } from './auth/firebase-auth.guard';
 
 async function bootstrap() {
+  const allowedOrigins = process.env.ALLOWED_ORIGINS;
   const app = await NestFactory.create(AppModule);
-  app.enableCors();
+  app.enableCors({
+    origin: allowedOrigins ? allowedOrigins.split(',') : '*', // Allow all origins if ALLOWED_ORIGINS is not set
+    methods: 'GET,HEAD,PUT,PATCH,POST,DELETE', // Allowed HTTP methods
+    credentials: true, // Allow credentials (cookies, authorization headers, etc.)
+  });
   app.useGlobalGuards(new FirebaseAuthGuard());
   await app.listen(process.env.PORT ?? 3001);
   console.log(`Application is running on: ${await app.getUrl()}`);
